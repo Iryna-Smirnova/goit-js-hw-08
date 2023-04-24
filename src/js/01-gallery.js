ce613f11fcda5ade35507b5aca6243bdd78ef2de
Iryna-Smirnova <irina_smirnova48@ukr.net>
2023-04-24 18:13:39 +0300

    add task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,50 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
 // Change code below this line
 
+const galleryList = document.querySelector('.gallery');
+
+const createGalleryItemMarkup = ({ preview, original, description }) => {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `;
+};
+
+const galleryItemsMarkup = galleryItems.map(createGalleryItemMarkup).join('');
+galleryList.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+galleryList.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const { nodeName, dataset, alt } = event.target;
+
+  if (nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${dataset.source}" alt="${alt}">
+`);
+
+  instance.show();
+
+  const onEscapePress = (event) => {
+    if (event.code === 'Escape') {
+      instance.close();
+      window.removeEventListener('keydown', onEscapePress);
+    }
+  };
+
+  window.addEventListener('keydown', onEscapePress);
+});
 console.log(galleryItems);
